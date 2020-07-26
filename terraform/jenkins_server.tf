fdc72b4adee2ea850f8bc347283337bebfabce89
luthan <luthan@gmail.com>
2020-07-26 21:35:19 +0300

    updated jenkins terraform file

--- terraform/jenkins_server.tf
+++ terraform/jenkins_server.tf
@@ -49,9 +49,18 @@ data "aws_iam_policy" "EC2FullAccess" {
   arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_server" {
+data "aws_iam_policy" "S3FullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_server_ec2_policy_attachment" {
   role       = aws_iam_role.jenkins_server.name
   policy_arn = data.aws_iam_policy.EC2FullAccess.arn
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_server_s3_policy_attachment" {
+  role       = aws_iam_role.jenkins_server.name
+  policy_arn = data.aws_iam_policy.S3FullAccess.arn
 }
 
 resource "aws_iam_instance_profile" "jenkins_server" {
